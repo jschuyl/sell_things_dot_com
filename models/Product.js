ac01c5cb6db65c3d45eba028822c0687391c1253
jschuyl <schuylertjed@gmail.com>
2022-11-27 15:20:20 -0700

    got models made, started creating the routes

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+    },
   },
   {
     sequelize,
